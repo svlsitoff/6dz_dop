b5c2f9ef10903e3729f690c79934ee77acff57ed
Vladimir Sarapulov <svlsitoff@gmail.com>
2017-04-07 19:55:45 +0800

    переделка сертификата

--- serteficate.php
+++ serteficate.php
@@ -1,16 +1,21 @@
 <?php
-header("Content-type: image/png");
+header("Content-type: image/png ");
+
+
 getimage(file_get_contents('serteficate.txt'));
+
+
 function getimage($string) {
 	 $img = imagecreatetruecolor(300, 260);
 	 $backcolor = imagecolorallocate($img, 255, 224, 221);
 	 $fontcolor = imagecolorallocate($img, 34, 75, 45);
-	 $fontfile = '/font/a_CampusOtl.ttf';
+	 $fontfile = 'font/a_CampusOtl.TTF';
 	 $imgbox = imagecreatefrompng('images/sert.png');
 	 imagefill($img, 0, 0, $backcolor);
 	 imagecopy($img, $imgbox, 10, 10, 0, 0, 400, 400);
-	 imagettftext($img, 16, 0, 20, 96, $fontcolor, $fontfile, file_get_contents('serteficate.txt'));
+	 imagettftext($img, 16, 0, 20, 96, $fontcolor, $fontfile, $string);
 	 imagepng($img);
 	 imagedestroy($img);
 
 }
+
